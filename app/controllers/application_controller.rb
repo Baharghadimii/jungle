0078e3614901aa8955ea8b3f3a3a5242bc1a7b76
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-12-12 17:27:00 -0800

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,13 @@ class ApplicationController < ActionController::Base
   private
 
   def cart
-    raise @cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
   helper_method :cart
 
   def enhanced_cart
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
-    puts "here"
-  
+      
   end
   helper_method :enhanced_cart
 
