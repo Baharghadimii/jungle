72ea0bfde2cd6f20973899f9e3622612751b8451
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-12-14 22:58:50 -0800

    add user authentication feature

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,4 +1,5 @@
 class AboutsController < ApplicationController
+  # before_filter :authorize
   def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,34 @@
 Rails.application.routes.draw do
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: [:create, :destroy]
+  end
+  
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    put    :add_item
+    delete :remove_item
   end
 
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, :categories, except: [:edit, :update, :show]
+    resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
   resource :about
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
